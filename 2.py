4c75f7d47c00aa09273d241665842cb7bc7d32b0
Viktor Kozhemiaka <9075276@gmail.com>
2024-07-29 11:21:23 +0300

    task 2 strt

---
+++ 2.py
@@ -0,0 +1,20 @@
+from collections  import deque
+
+def is_palindrome(imp_str: str) -> bool:
+    
+    # Приводимо символи до нижнього регістру, видаляємо пробіли
+    imp_str = imp_str.lower().replace(' ', '')
+    
+    # Додаємо символи до двосторонньої черги
+    d = deque(imp_str)
+    
+    # Перевіряємо чергу на поліндром
+    while len(d)>1:
+        if d.popleft() != d.pop():
+            return False
+    return True    
+
+print(is_palindrome("Козак з казок"))  
+print(is_palindrome("123 3 2 1"))
+print(is_palindrome("1234 3 2 1"))
+print(is_palindrome("124 3 2 1"))
